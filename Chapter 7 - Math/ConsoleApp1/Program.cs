766607b713dd19be8b3a5ed5edbda0e9b1b4b106
Dan Soares <dan.soaresn@gmail.com>
2025-07-08 12:01:57 -0300

    The triangle farmer
    chapter 7 first challenge

---
+++ Chapter 7 - Math/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double CalculateArea(double baseLength, double height)
+{
+    double area = 0.5 * baseLength * height;
+    return area;
+}
+
+Console.WriteLine("Write the Base");
+string TBase = Console.ReadLine();
+double TBaseNumber = double.Parse(TBase);
+Console.WriteLine("Write the Height");
+string THeight = Console.ReadLine();
+double THeightNumber = double.Parse(THeight);
+
+Console.WriteLine("The Area of the Triangle is: " + CalculateArea(THeightNumber, TBaseNumber));
