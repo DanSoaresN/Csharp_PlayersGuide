b7ac36c3df5678ce9fa34c45b667be2fea9a64dd
Dan Soares <98857555+DanSoaresN@users.noreply.github.com>
2025-07-16 10:00:19 -0300

    2 more challenges

---
+++ Chapter 7 - Math/The Dominion Of Kings/Program.cs
@@ -0,0 +1,11 @@
+﻿int provinces, duchies, estates;
+
+Console.WriteLine("How many provinces are there?");
+provinces = int.Parse(Console.ReadLine());
+Console.WriteLine("How many dutchies are there?");
+duchies = int.Parse(Console.ReadLine());
+Console.WriteLine("How many estates are there?");
+estates = int.Parse(Console.ReadLine());
+
+int totalPoints = (provinces * 6) + (duchies * 3) + estates;
+Console.WriteLine($"Total points: {totalPoints}");    
---
+++ Chapter 7 - Math/The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("How many chocolate eggs was gathered?");
+int chocolateEggs = int.Parse(Console.ReadLine());
+
+int sistersEggs = chocolateEggs / 4;
+int duckbearEggs = chocolateEggs % 4;
+
+Console.WriteLine($"Each sister gets {sistersEggs} chocolate eggs.");
+Console.WriteLine($"The duckbear gets {duckbearEggs} chocolate eggs.");
