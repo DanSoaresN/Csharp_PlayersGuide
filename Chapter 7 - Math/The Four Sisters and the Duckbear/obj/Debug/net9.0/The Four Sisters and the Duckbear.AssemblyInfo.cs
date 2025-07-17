ac51266124a8e2e190f46f1155a841688bc23fc6
Dan Soares <98857555+DanSoaresN@users.noreply.github.com>
2025-07-17 16:21:06 -0300

    the defense of the consolas

--- Chapter 7 - Math/The Four Sisters and the Duckbear/obj/Debug/net9.0/The Four Sisters and the Duckbear.AssemblyInfo.cs
+++ Chapter 7 - Math/The Four Sisters and the Duckbear/obj/Debug/net9.0/The Four Sisters and the Duckbear.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The Four Sisters and the Duckbear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ac36c3df5678ce9fa34c45b667be2fea9a64dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c30483075795368b846229dc1bb321b7311a94f")]
 [assembly: System.Reflection.AssemblyProductAttribute("The Four Sisters and the Duckbear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The Four Sisters and the Duckbear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Chapter 8 - Console 2.0/The Defense of Consolas/Program.cs
@@ -0,0 +1,25 @@
+﻿int row, col;
+
+Console.Title = "The Defense of Consolas";
+
+Console.Write("Target row: ");
+row = int.Parse(Console.ReadLine());
+Console.Write("Target column: ");
+col = int.Parse(Console.ReadLine());
+
+if (row < 2 || row > 7 || col < 2 || col > 7)
+{
+    Console.WriteLine("Invalid target position. Please enter values between 2 and 7.");
+    return;
+}
+else
+{
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep();
+
+    Console.WriteLine($"Deploy to: {row}, {--col}");
+    Console.WriteLine($"Deploy to: {--row}, {++col}");
+    Console.WriteLine($"Deploy to: {++row}, {++col}");
+    Console.WriteLine($"Deploy to: {++row}, {--col}");
+}
